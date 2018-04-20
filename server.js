32206677029ddf748b60a7d185afc881422389b1
Debabrata Pal <paldebabrata82@gmail.com>
2018-04-20 23:24:54 +0530

    ready to deploy

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs')
+const port = process.env.PORT || 3000;
 
 var app = express();
+
+
 
 hbs.registerPartials(__dirname+'/views/partials')
 app.set('view engine', 'hbs');
@@ -49,6 +52,6 @@ app.get('/about', (req, res) => {
   });
 })
 
-app.listen(3000, () => {
-  console.log('Server is started on Local Host 3000');
+app.listen(port, () => {
+  console.log(`Server is started on Local Host ${port}`);
 });
